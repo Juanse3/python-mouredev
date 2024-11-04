2299fc8413e821e154e6c6bdc456f8aa1e5d3389
juanse <Juanse>
2024-11-03 21:55:55 -0300

    primeros pasos. hola mundo y comentarios

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Programa de Hola Mundo
+# Tipo de comentario de una linea 
+"""
+Comentario 
+en 
+varias 
+lineas
+"""
+print("Hola mundo doble") # string con comillas dobles
+print('Hola mundo simple') # string con comillas simples
