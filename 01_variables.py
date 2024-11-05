0065bdeb38c1ef954db3890328eb46dd54d0b9d0
juanse <Juanse>
2024-11-04 22:54:34 -0300

    hasta tiempo de video 3

--- 01_variables.py
+++ 01_variables.py
@@ -22,3 +22,33 @@ print(my_float_var)
 
 
 print(my_variable,":",my_float_var) #print con multiples inputs
+
+print(len(my_variable)) # len entrega el largo de una cadena
+
+#varibles en una sola linea
+
+name, surname, age, work = "Juan", "Muñoz", 42, True
+print(surname,",",name, "| Edad:",age,"| Has Work:", work)  #agrega espacios despues de cada variable
+
+#ingreso de datos por el usuario
+
+name = input("Nombre?")
+age = input("Edad?")
+
+print( name, "tiene", age,"años de edad.")
+
+#cambio de tipo de dato
+name = 43
+age = False
+
+print(name)
+print(age)
+
+
+#explicitamos tipo de dato que se quiere en la variable, pero igual puede cambiar su tipo
+
+apellido:str="Lopez"
+print(apellido)
+apellido=True
+print(apellido)
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-mouredev
  Tiempo video 1: 0:49
  Tiempo video 2: 1:08 
+ Tiempo video 3: 1:33
